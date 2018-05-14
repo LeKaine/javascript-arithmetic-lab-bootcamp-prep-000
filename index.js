4284007f0094f8ac15b4d15323eec18c6c2276aa
Daniel Hoellger <hoellgerdaniel92@gmail.com>
2018-05-14 00:06:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,7 @@ function subtract(a,b){
 function multiply (a,b){
  return a * b;
 }
-function multiply (a,b){
- return a * b;
-}
 
 function divide (a,b){
  return a / b;
-}
-
-var number = 5{
-number++
-return number;
 }
